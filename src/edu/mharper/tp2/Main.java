6dd9d5c86bdd6bd068418c97d906c58e10d7f114
m-harper <matthew.harper@tamu.edu>
2013-03-06 16:56:24 -0600

    Finished drawing board lines, fixed spacing issues with menubar

--- src/edu/mharper/tp2/Main.java
+++ src/edu/mharper/tp2/Main.java
@@ -1,17 +1,19 @@
 package edu.mharper.tp2;
 
-import java.awt.Dimension;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 public class Main {
 	
-	public static int windowHeight = 600;
-	public static int windowWidth = 800;
+	
+	//public static int windowWidth = 800;
+	//public static int windowHeight = 600;
 	public static String gameTitle = "Fanorona";
 	public static int horizontalSpaces = 9;
 	public static int verticalSpaces = 5;
-
+	public static int tileSize = 100;
+	public static int menuBarSize = 25;
+	public static int displayInfoSize = 150;
+	public static int windowHeight = Main.verticalSpaces * Main.tileSize + 51; // 51 is offset constant 
+	public static int windowWidth = Main.horizontalSpaces * Main.tileSize + 6; // 6 is offset constant
+	
 	public static void main(String[] args) {
 
 		View view = new View();
--- src/edu/mharper/tp2/View.java
+++ src/edu/mharper/tp2/View.java
@@ -1,22 +1,29 @@
 package edu.mharper.tp2;
 
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.MenuBar;
+import java.awt.Shape;
+import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 import javax.swing.JFrame;
 
 public class View extends Canvas implements ActionListener {
 	
 	JFrame frame;
+	JPanel panel;
 	JMenuBar menuBar;
 	JMenu fileMenu;
 	JMenuItem newGameMenuItem;
@@ -25,6 +32,7 @@ public class View extends Canvas implements ActionListener {
 	public View() {
 		frame = new JFrame(Main.gameTitle);
 		menuBar = new JMenuBar();
+		panel = new JPanel(new BorderLayout());
 		fileMenu = new JMenu("File");
 		newGameMenuItem = new JMenuItem("New game");
 		exitMenuItem = new JMenuItem("Exit");
@@ -42,8 +50,9 @@ public class View extends Canvas implements ActionListener {
 		menuBar.add(fileMenu);
 		
 		// Add the items to the window
+		panel.add(this);
 		frame.setJMenuBar(menuBar);
-		frame.add(this);
+		frame.add(panel);
 		
 		
 		frame.pack();
@@ -53,6 +62,7 @@ public class View extends Canvas implements ActionListener {
 	public void paint(Graphics g) {
 		drawBackground(g);
 		drawSpaces(g);
+		drawLines(g);
 	}
 	
 	private void drawBackground(Graphics g) {
@@ -61,16 +71,67 @@ public class View extends Canvas implements ActionListener {
 	}
 	
 	private void drawSpaces(Graphics g) {
-		int spaceWidth = 50;
-		double horizontalSpacing = Main.windowWidth / Main.horizontalSpaces;
-		double verticalSpacing = Main.windowHeight / Main.verticalSpaces;
+		Dimension panelSize = getParent().getSize();
+		System.out.println(panelSize);
+		double horizontalSpacing = panelSize.getWidth() / Main.horizontalSpaces;
+		double verticalSpacing = panelSize.getHeight() / Main.verticalSpaces;
+		
 		
 		g.setColor(Color.white);
 		for (int i = 0; i < Main.horizontalSpaces; i++) {
 			for (int j = 0; j < Main.verticalSpaces; j++) {
-				g.fillOval((int) (i * horizontalSpacing + spaceWidth / 2 - 15), (int) (j * verticalSpacing + spaceWidth / 2 - 15), spaceWidth, spaceWidth);
+				g.fillOval((int) (i * horizontalSpacing), (int) (j * verticalSpacing), (int) horizontalSpacing, (int) verticalSpacing);
 			}
 		}
+	}
+	
+	private void drawLines(Graphics g) {
+		int spacing = Main.tileSize / 2;
+		
+		// Set up drawing properties
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.blue);
+		g2.setStroke(new BasicStroke(5));
+		
+		// Draw vertical lines
+		for (int i = 0; i < Main.horizontalSpaces; i++) {
+			g2.drawLine(i * Main.tileSize + spacing, spacing, i * Main.tileSize + spacing, getParent().getHeight() - spacing);
+		}
+		
+		// Draw horizontal lines
+		for (int i = 0; i < Main.verticalSpaces; i++) {
+			g2.drawLine(spacing, i * Main.tileSize + spacing, getParent().getWidth() - spacing, i * Main.tileSize + spacing);
+		}
+		
+		// Draw right diagonals
+		for (int i = 0; i < Main.horizontalSpaces; i++) {
+			// Draw every other space diagonal
+			if (i % 2 == 0 && i + 4 < Main.horizontalSpaces) {
+				int xStart = i * Main.tileSize + spacing;
+				int xEnd = (i + 4) * Main.tileSize + spacing;
+				int yStart = spacing;
+				int yEnd = getParent().getHeight() - spacing;
+				g2.drawLine(xStart, yStart, xEnd, yEnd);
+			}
+		}
+		// Draw shorter right diagonal
+		g2.drawLine(getParent().getWidth() - (2 * Main.tileSize + spacing) , spacing, getParent().getWidth() - spacing, Main.tileSize * 2 + spacing);
+		g2.drawLine(spacing, 2 * Main.tileSize + spacing, 2 * Main.tileSize + spacing, getParent().getHeight() - spacing);
+		
+		// Draw left diagonals
+		for (int i = Main.horizontalSpaces; i >= 0; i--) {
+			// Draw every other space diagonal
+			if (i % 2 == 0 && i - 4 >= 0) {
+				int xStart = i * Main.tileSize + spacing;
+				int xEnd = (i - 4) * Main.tileSize + spacing;
+				int yStart = spacing;
+				int yEnd = getParent().getHeight() - spacing;
+				g2.drawLine(xStart, yStart, xEnd, yEnd);
+			}
+		}
+		// Draw shorter left diagonal
+		g2.drawLine(2 * Main.tileSize + spacing, spacing, spacing, 2 * Main.tileSize + spacing);
+		g2.drawLine(getParent().getWidth() - spacing, 2 * Main.tileSize + spacing, getParent().getWidth() - 2 * Main.tileSize - spacing, getParent().getHeight() - spacing);
 	}
 
 	@Override
