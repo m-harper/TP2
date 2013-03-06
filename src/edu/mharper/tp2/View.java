6cb08d75e30777f2dbd835e5ac8d4f085e39951f
m-harper <matthew.harper@tamu.edu>
2013-03-05 19:54:04 -0600

    Added basic menu

--- src/edu/mharper/tp2/View.java
+++ src/edu/mharper/tp2/View.java
@@ -4,20 +4,48 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.MenuBar;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import javax.swing.JFrame;
 
-public class View extends Canvas {
+public class View extends Canvas implements ActionListener {
 	
 	JFrame frame;
+	JMenuBar menuBar;
+	JMenu fileMenu;
+	JMenuItem newGameMenuItem;
+	JMenuItem exitMenuItem;
 	
 	public View() {
 		frame = new JFrame(Main.gameTitle);
+		menuBar = new JMenuBar();
+		fileMenu = new JMenu("File");
+		newGameMenuItem = new JMenuItem("New game");
+		exitMenuItem = new JMenuItem("Exit");
+		
+		// Set up window
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setPreferredSize(new Dimension(Main.windowWidth, Main.windowHeight));
-		// Add the canvas
+		
+		// Set up menu
+		newGameMenuItem.addActionListener(this);
+		exitMenuItem.addActionListener(this);
+		fileMenu.add(newGameMenuItem);
+		fileMenu.add(exitMenuItem);
+		menuBar.add(fileMenu);
+		
+		// Add the items to the window
+		frame.setJMenuBar(menuBar);
 		frame.add(this);
+		
+		
 		frame.pack();
 		frame.setVisible(true);
 	}
@@ -40,9 +68,22 @@ public class View extends Canvas {
 		g.setColor(Color.white);
 		for (int i = 0; i < Main.horizontalSpaces; i++) {
 			for (int j = 0; j < Main.verticalSpaces; j++) {
-				g.fillOval((int) (i * horizontalSpacing + spaceWidth / 2), (int) (j * verticalSpacing + spaceWidth / 2), spaceWidth, spaceWidth);
+				g.fillOval((int) (i * horizontalSpacing + spaceWidth / 2 - 15), (int) (j * verticalSpacing + spaceWidth / 2 - 15), spaceWidth, spaceWidth);
 			}
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if (event.getActionCommand().equals("New game")) {
+			// New game
+			// Tell the game logic to reset the board
+			this.update(getGraphics());
+		}
+		else if (event.getActionCommand().equals("Exit")) {
+			System.exit(0);
+		}
+		
 	}
 	
 	
