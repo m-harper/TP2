c4cb2223cbfe67fcc61438ed61568339f930c295
m-harper <matthew.harper@tamu.edu>
2013-03-25 17:14:22 -0500

    Option to start as server or client

--- src/edu/mharper/tp2/Main.java
+++ src/edu/mharper/tp2/Main.java
@@ -1,5 +1,7 @@
 package edu.mharper.tp2;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 	
 	
@@ -9,15 +11,20 @@ public class Main {
 	public static int verticalSpaces = 5;
 	public static int tileSize = 100;
 	public static int pieceSize = tileSize / 2;
-	//public static int menuBarSize = 25;
 	public static int displayInfoSize = 150;
 	public static int windowHeight = Main.verticalSpaces * Main.tileSize;  
 	public static int windowWidth = Main.horizontalSpaces * Main.tileSize; 
 	
 	public static void main(String[] args) {
-
-		//GameBoard gb = new GameBoard();
-		//GameManager manager = new GameManager();
+		int choice = JOptionPane.showOptionDialog(null, "Start as server?", "Server setup", JOptionPane.YES_NO_OPTION, 1, null, null, null);
+		switch (choice) {
+		case 0:
+			System.out.println("Started as server");
+			break;
+		case 1:
+			System.out.println("Started as client");
+			break;
+		}
 		View view = new View();
 		//view.updatePieces(gb.getPieces());
 	}
