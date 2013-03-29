6857b25cd678cd3a6856a553bd87e41b6ecf85bd
m-harper <matthew.harper@tamu.edu>
2013-03-29 12:00:09 -0500

    Settings intro screen, board size correctly draws

--- src/edu/mharper/tp2/Main.java
+++ src/edu/mharper/tp2/Main.java
@@ -15,17 +15,7 @@ public class Main {
 	public static int defaultTime = 5000;
 	
 	public static void main(String[] args) {
-		
-		int choice = JOptionPane.showOptionDialog(null, "Start as server?", "Server setup", JOptionPane.YES_NO_OPTION, 1, null, null, null);
-		switch (choice) {
-		case 0:
-			System.out.println("Started as server");
-			break;
-		case 1:
-			System.out.println("Started as client");
-			break;
-		}
-		View view = new View();
+		Settings view = new Settings();
 		//view.updatePieces(gb.getPieces());
 	}
 
